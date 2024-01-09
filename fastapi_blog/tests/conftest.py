8e8493870b1f1763845ea4e8ce69080f5d1a4e5f
Vladislav <gordin@it-impuls.ru>
2024-01-09 16:04:37 +0500

    настройка тестирования

--- fastapi_blog/tests/conftest.py
+++ fastapi_blog/tests/conftest.py
@@ -3,32 +3,36 @@ from typing import AsyncGenerator
 
 import pytest
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from tests.my_types import FixtureFunctionT
 
-from webapp.db.postgres import engine
 from webapp.main import create_app
 from webapp.models import meta
 
+TEST_DB_URL = 'postgresql+asyncpg://postgres:postgres@web_db:5432/test_db'
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture(scope='session')
 async def app(_migrate_db: FixtureFunctionT) -> FastAPI:
     return create_app()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def event_loop():
     return asyncio.get_event_loop()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 async def _migrate_db() -> AsyncGenerator[None, None]:
-    async with engine.begin() as conn:
+    test_engine = create_async_engine(TEST_DB_URL)
+
+    async with test_engine.begin() as conn:
         await conn.run_sync(meta.metadata.create_all)
 
     yield
 
-    async with engine.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(meta.metadata.drop_all)
 
 
@@ -37,15 +41,17 @@ async def _migrate_db() -> AsyncGenerator[None, None]:
 
 # Фикстура app создает экземпляр FastAPI-приложения, используя функцию create_app() из
 # модуля webapp.main. Перед созданием приложения выполняется фикстура _migrate_db,
-# которая создает все необходимые таблицы в базе данных. После выполнения тестов фикстура
-# _migrate_db удаляет все таблицы из базы данных.
+# которая создает все необходимые таблицы в базе данных. После выполнения тестов
+# фикстура _migrate_db удаляет все таблицы из базы данных.
 
-# Фикстура event_loop возвращает экземпляр цикла событий asyncio, который используется для
-# асинхронной обработки запросов в приложении.
+# Фикстура event_loop возвращает экземпляр цикла событий asyncio, который
+# используется для асинхронной обработки запросов в приложении.
 
-# Фикстура _migrate_db создает и удаляет таблицы в базе данных Postgres. Она используется
-# в фикстуре app для создания приложения с уже существующими таблицами в базе данных.
+# Фикстура _migrate_db создает и удаляет таблицы в базе данных Postgres.
+# Она используется в фикстуре app для создания приложения с уже
+# существующими таблицами в базе данных.
 
-# В целом, данный код предназначен для подготовки окружения для тестирования FastAPI-приложения
-# с использованием базы данных Postgres. Он позволяет создавать и удалять таблицы в базе данных
-# перед и после выполнения тестов, а также создавать экземпляр приложения для тестирования его функциональности.
+# В целом, данный код предназначен для подготовки окружения для тестирования
+# FastAPI-приложения с использованием базы данных Postgres. Он позволяет
+# создавать и удалять таблицы в базе данных перед и после выполнения тестов,
+# а также создавать экземпляр приложения для тестирования его функциональности.
